2f1793fecebb072bc8cf94cf867737d7c6f92f24
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-12-26 16:09:46 -0300

    creating food

--- snake.py
+++ snake.py
@@ -1,19 +1,47 @@
 import pygame
 import sys
+from pygame.math import Vector2
+import random
 
 pygame.init()
 
-screen = pygame.display.set_mode((750, 750))
+GREEN = (173, 204, 96)
+DARK_GREEN = (43, 51, 24)
+
+cell_size = 30
+number_of_cells = 25
+
+class Food:
+    def __init__(self):
+        self.position = self.generate_random_position()
+
+    def draw(self):
+        food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
+        screen.blit(food_surface, food_rect)
+
+    def generate_random_position(self):
+        x = random.randint(0, number_of_cells - 1)
+        y = random.randint(0, number_of_cells - 1)
+        position = Vector2(x, y)
+        return position
+
+screen = pygame.display.set_mode((cell_size * number_of_cells, cell_size * number_of_cells))
 
 pygame.display.set_caption("Retro Snake")
 
 clock = pygame.time.Clock()
+
+food = Food()
+food_surface = pygame.image.load('graphics/food.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.fill(GREEN)
+    food.draw()
 
     pygame.display.update()
     clock.tick(60)
